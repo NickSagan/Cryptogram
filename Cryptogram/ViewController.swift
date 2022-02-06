c350390254a8741ca1f8f8f2109773718fc0b41b
NickSagan <9952728@gmail.com>
2022-02-06 19:56:23 +0300

    ready encrypt logic

--- Cryptogram/ViewController.swift
+++ Cryptogram/ViewController.swift
@@ -9,16 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let key: String = "44 52 d7 16 87 b6 bc 2c 93 89 c3 34 9f dc 17 fb 3d fb ba 62 24 af fb 76 76 e1 33 79 26 cd d6 02"
     
     @IBOutlet weak var inputText: UITextView!
     @IBOutlet weak var outputText: UITextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
     }
     
     @IBAction func encryptPressed(_ sender: UIButton) {
+        let text = inputText.text!
+        var encryptedText: String?
+        do {
+            let tryToEncrypt: String? = try Encryption.encryptData(plainText: text, hexKey: key)
+            encryptedText = tryToEncrypt
+        } catch {
+            print(error)
+        }
+        
+        outputText.text = encryptedText
     }
     
     @IBAction func decryptPressed(_ sender: UIButton) {
