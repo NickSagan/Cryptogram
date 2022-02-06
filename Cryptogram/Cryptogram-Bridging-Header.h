47162d8100014eef482e163ac7c3db16a5ca57b4
NickSagan <9952728@gmail.com>
2022-02-06 19:28:55 +0300

    add Bridging-Header.h

---
+++ Cryptogram/Cryptogram-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Cryptogram-ad
+//  Cryptogram
+//
+//  Created by Nick Sagan on 06.02.2022.
+//
+
+#ifndef Cryptogram_Bridging_Header_h
+#define Cryptogram_Bridging_Header_h
+
+#import <CommonCrypto/CommonCryptor.h>
+#import <CommonCrypto/CommonHMAC.h>
+
+#endif /* Cryptogram_Bridging_Header_h */
