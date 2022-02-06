1928580855dfdf6c4d8964e7d9841c517acd94b4
NickSagan <9952728@gmail.com>
2022-02-06 19:40:48 +0300

    IBOutlets & IBActions

--- Cryptogram/ViewController.swift
+++ Cryptogram/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var inputText: UITextView!
+    @IBOutlet weak var outputText: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func encryptPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func decryptPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func sendEncryptedText(_ sender: UIButton) {
+    }
+    
+    @IBAction func sendKey(_ sender: UIButton) {
+    }
+    
 
 }
 
