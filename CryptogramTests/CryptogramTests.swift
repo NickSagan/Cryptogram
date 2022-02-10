6785934dee17739215a80908723e1f35d32f4cda
NickSagan <9952728@gmail.com>
2022-02-10 14:29:31 +0300

    add XCTEST func testKeyGenerator

--- CryptogramTests/CryptogramTests.swift
+++ CryptogramTests/CryptogramTests.swift
@@ -10,21 +10,12 @@ import XCTest
 
 class CryptogramTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-    
     func testTextViewPlaceholders() throws {
         let mv = MainView()
         XCTAssertTrue(!mv.outputPlaceholder.isEmpty)
         XCTAssertTrue(!mv.inputPlaceholder.isEmpty)
         XCTAssertEqual(mv.input.text, "Paste the text you want to encrypt or decrypt here")
         XCTAssertEqual(mv.output.text, "The decrypted or encrypted text will appear here")
-        
     }
     
     func testPlaceholdersColoursAfterViewDidLoad() throws {
@@ -32,13 +23,6 @@ class CryptogramTests: XCTestCase {
         vc.viewDidLoad()
         XCTAssertEqual(vc.mainView.output.textColor, UIColor.lightGray)
         XCTAssertEqual(vc.mainView.input.textColor, UIColor.lightGray)
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
     }
 
 }
--- CryptogramUITests/CryptogramUITests.swift
+++ CryptogramUITests/CryptogramUITests.swift
@@ -22,13 +22,13 @@ class CryptogramUITests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // UI tests must launch the application that they test.
+    func testKeyGeneratorButtonAndTextView() throws {
         let app = XCUIApplication()
         app.launch()
-
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let tv = app.textViews["keyTextView"]
+        XCTAssertEqual((tv.value as! String).count, 64)
+        app.buttons["generateKey"].tap()
+        XCTAssertEqual((tv.value as! String).count, 64)
     }
 
     func testLaunchPerformance() throws {
